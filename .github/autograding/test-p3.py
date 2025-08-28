d5f6d6e59f05f78c7859e2dc02bee5e4dcaf3563
Chi-kwan Chan <chanc@arizona.edu>
2025-08-28 02:25:16 -0700

    Create more extensive tests

--- .github/autograding/test-p3.py
+++ .github/autograding/test-p3.py
@@ -1,8 +1,29 @@
 # This is a pytest script to test quadratic() from part 3.
 # This function should be imported from the solution file.
 
-from math import isclose
 from hw1.p3 import quadratic
+
+# -----------------------------------------------------------------------------
+# Helper functions
+
+from math import isclose
+
+def agree(x, t):
+    if t is None:
+        return x is None
+    else:
+        return isclose(x, t)
+
+def compare(s, t1, t2, a=1, b=2, c=1):
+    x1, x2 = quadratic(a, b, c)
+
+    print(s)
+    print(f"{a=}, {b=}, {c=}")
+    print(f"{t1=}, {t2=}")
+    print(f"{x1=}, {x2=}")
+
+    assert agree(x1, t1)
+    assert agree(x2, t2)
 
 # -----------------------------------------------------------------------------
 # Test functions for quadratic().
@@ -12,48 +33,26 @@ def test_catastrophic_cancellation():
     accurately both roots when catastrophic cancellation may happen.
 
     """
-    t1, t2  = 1e-8, 1e8
-    a, b, c = 1.0, -t1-t2, 1.0
-    x1, x2  = quadratic(a, b, c)
-
-    print("Catastrophic Cancellation")
-    print(f"{a=}, {b=}, {c=}")
-    print(f"{t1=}, {t2=}")
-    print(f"{x1=}, {x2=}")
-
-    assert isclose(x1, t1)
-    assert isclose(x2, t2)
+    for t1 in [-1e8, -1e7, -1e6, -1e5, -1e4, 1e4, 1e5, 1e6, 1e7, 1e8]:
+        t2 = 1.0 / t1
+        b  = -t1-t2
+        compare("Catastrophic Cancellation", min(t1, t2), max(t1, t2), b=b)
 
 def test_one_root():
     """Test the quadratic() function by verifying x2 == None when
     there is only one real root.
 
     """
-    t1, t2  = 1.0, None
-    a, b, c = 1.0, -2.0, 1.0
-    x1, x2  = quadratic(a, b, c)
-
-    print("One Root")
-    print(f"{a=}, {b=}, {c=}")
-    print(f"{t1=}, {t2=}")
-    print(f"{x1=}, {x2=}")
-
-    assert isclose(x1, t1)
-    assert x2 is None
+    for t1 in range(10):
+        b = -2 * t1
+        c = t1 * t1
+        compare("One Root", t1, None, b=b, c=c)
 
 def test_no_root():
     """Test the quadratic() function by verifying x1 == x2 == None
     when there is no real root.
 
     """
-    t1, t2  = None, None
-    a, b, c = 1.0, -1.0, 1.0
-    x1, x2  = quadratic(a, b, c)
-
-    print("No Root")
-    print(f"{a=}, {b=}, {c=}")
-    print(f"{t1=}, {t2=}")
-    print(f"{x1=}, {x2=}")
-
-    assert x1 is None
-    assert x2 is None
+    for b in [-1e-0, -1e-1, -1e-2, -1e-3, -1e-4,
+               1e-4,  1e-3,  1e-2,  1e-1,  1e-0]:
+        compare("No Root", None, None, b=b)
