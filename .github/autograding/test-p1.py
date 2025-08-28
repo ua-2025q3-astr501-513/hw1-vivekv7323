b96ea16a04943a968b16a0d8a2becf5b4c02ad78
Chi-kwan Chan <chanc@arizona.edu>
2025-08-27 23:42:03 -0700

    Make sure we also test for LICENSE in Part 1

--- .github/autograding/test-p1.py
+++ .github/autograding/test-p1.py
@@ -4,6 +4,8 @@
 # (pyproject.toml, etc.) are set up correctly so that the required
 # submodules can be imported.
 
+from pathlib import Path
+
 def test_import():
     """Test whether the module structure is correctly configured for
     imports.
@@ -19,3 +21,6 @@ def test_import():
         from hw1 import p2, p3, p4  # import required submodules
     except ImportError as e:
         assert False, f"Module import failed: {e}"
+
+    if not Path('LICENSE').is_file():
+        assert False, "'LICENSE' does not exist"
